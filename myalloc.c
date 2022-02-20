3e1ef3abddf52bc9e4b730ea9a1272616b34c590
Caleb Knight <knightca@oregonstate.edu>
2022-02-20 11:06:27 -0800

    Had to move some methods around to declare properly

--- myalloc.c
+++ myalloc.c
@@ -27,33 +27,6 @@ struct block {
     int in_use;   // Boolean
 };
 
-void *myalloc(int size)
-{
-	void *h = sbrk(1024);
-
-	size_t padded_block_size = PADDED_SIZE(sizeof(struct block));
-
-	if(head == NULL) {
-		head = h;
-		head -> next = NULL;
-		head -> size = 1024 - padded_block_size;
-		head -> in_use = 0;
-	}
-	struct block *n = head;
-
-	while(n != NULL){
-		if(n -> size >= size && n -> in_use == 0){
-			split_space(n, size);
-			n->in_use = 1;
-			return PTR_OFFSET(n, PADDED_SIZE(sizeof(struct block)));
-		}
-		n = n -> next;
-	}
-	return NULL;
-}
-
-int required_space = PADDED_SIZE(bytes) + PADDED_SIZE(sizeof(struct block)) + 16;
-
 void find_space(int bytes) 
 {
 	bytes = bytes + GET_PAD(bytes);
@@ -68,60 +41,92 @@ void find_space(int bytes)
 	return NULL;
 }
 
-
-
 // Split_Space(current_node, requested_size):
     // If current_node big enough to split:
         // Add a new struct block with the remaining unused space
         // Wire it into the linked list
 
+int required_space = PADDED_SIZE(bytes) + PADDED_SIZE(sizeof(struct block)) + 16;
+
 void split_space(struct block *n, int bytes)
 {
-	if (n -> size >= required_space){
-		struct block *new_n = PTR_OFFSET(n, PADDED_SIZE(bytes) + PADDED_SIZE(sizeof(struct block))); // create new node
-		new_n -> in_use = 0; // declare not in use
-		new_n -> size = n->size - (PADDED_SIZE(bytes) + (PADDED_SIZE(sizeof(struct block)))); // declares start of new block
-		new_n -> next = NULL; // set value of next to NULL
-		n -> next = new_n; // make the next node new
+		struct block *new_node = PTR_OFFSET(n, PADDED_SIZE(bytes) + PADDED_SIZE(sizeof(struct block))); // create new node
+		new_node -> in_use = 0; // declare not in use
+		new_node -> size = n->size - (PADDED_SIZE(bytes) + (PADDED_SIZE(sizeof(struct block)))); // declares start of new block
+		new_node -> next = NULL; // set value of next to NULL
+		n -> next = new_node; // make the next node new
 		n -> size = PADDED_SIZE(bytes); // sets up next
 	}
-}
+	void *myalloc(int size)
+	{
+		void *h = sbrk(1024);
 
-void print_data(void)
-{
-	struct block *b = head;
+		size_t padded_block_size = PADDED_SIZE(sizeof(struct block));
 
-	if (b == NULL) {
-		printf("[empty]\n");
-		return;
+		if(head == NULL) {
+			head = h;
+			head -> next = NULL;
+			head -> size = 1024 - padded_block_size;
+			head -> in_use = 0;
+		}
+		struct block *n = head;
+
+		while(n != NULL){
+			if(n -> size >= size && n -> in_use == 0){
+				if (n -> size >= required_space){
+					split_space(n, space);
+				}
+				n->in_use = 1;
+				return PTR_OFFSET(n, PADDED_SIZE(sizeof(struct block)));
+			}
+			n = n -> next;
+		}
+		return NULL;
 	}
 
-	while (b != NULL) {
-        // Uncomment the following line if you want to see the pointer values
-        // printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
-		printf("[%d,%s]", b->size, b->in_use? "used": "free");
-		if (b -> next != NULL) {
-			printf(" -> ");
+	void my_free(void *p) 
+	{
+		struct block *h = head;
+		if ((int)p == (int)(h - (GET_PAD(sizeof(struct block))) + 1)){
+			h -> in_use = 0;
+		} h = h -> next;	
+	}
+
+	void print_data(void)
+	{
+		struct block *b = head;
+
+		if (b == NULL) {
+			printf("[empty]\n");
+			return;
 		}
 
-		b = b -> next;
+		while (b != NULL) {
+        // Uncomment the following line if you want to see the pointer values
+        // printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+			printf("[%d,%s]", b->size, b->in_use? "used": "free");
+			if (b -> next != NULL) {
+				printf(" -> ");
+			}
+
+			b = b -> next;
+		}
+
+		printf("\n");
 	}
 
-	printf("\n");
-}
-
-int main(void) {
+	int main(void) {
 	// void *p;
 
 	// print_data();
 	// p = myalloc(64);
 	// print_data();
 
-	void *p;
+		void *p;
 
-	print_data();
-	p = myalloc(16);
-	print_data();
-	p = myalloc(16);
-	printf("%p\n", p);
-}
+		print_data();
+		p = myalloc(16);
+		print_data();
+		p = myalloc(16);
+		printf("%p\n", p);
+	}
